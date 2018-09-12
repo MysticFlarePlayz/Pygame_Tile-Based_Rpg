818502436d9d0f9f8ac37e10c72bcdf200ec8e0c
MysticFlarePlayz <42909725+MysticFlarePlayz@users.noreply.github.com>
2018-09-12 20:48:53 +0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -16,8 +16,11 @@ class Game:
         p.init()
         self.screen = p.display.set_mode((WIDTH, HEIGHT))
         p.display.set_caption("2D Rpg")
+        p.key.set_repeat(500, 100)
+    def gamestart(self):
         self.sprites = p.sprite.Group()
-        self.sprites.add(player)
+        self.player = Player()
+        self.player.add(self.sprites)
     def run(self):
         while not self.exit:
             g.events()
@@ -42,7 +45,15 @@ class Game:
             if event.type ==  p.QUIT:
                 self.exit = True
                 quit()
-
+            if event.type == p.KEYDOWN:
+                if event.key == p.K_LEFT:
+                    self.player.move(x=-1)
+                if event.key == p.K_RIGHT:
+                    self.player.move(x=1)
+                if event.key == p.K_UP:
+                    self.player.move(y=-1)
+                if event.key == p.K_DOWN:
+                    self.player.move(y=1)
     def draw_grid(self):
         for x in range(0, WIDTH, TILESIZE):
             p.draw.line(self.screen, lightgrey, (x, 0), (x, HEIGHT))
@@ -51,5 +62,6 @@ class Game:
 g = Game()
 
 while 1:
+    g.gamestart()
     g.run()
 
